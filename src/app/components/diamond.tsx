913bab9ff267e7621990f6fea5099bd668c89ae7
solensa <39489819+solensa@users.noreply.github.com>
2025-07-01 02:06:04 +0100

    iframe v2

--- src/app/components/diamond.tsx
+++ src/app/components/diamond.tsx
@@ -179,7 +179,7 @@ export default function Diamond() {
 
   return (
     <>
-      <div className="min-h-screen p-4 sm:p-8">
+      <div className="p-4 sm:p-8">
         <div className="max-w-4xl mx-auto">
           <div className="flex flex-col items-center justify-center -rotate-45">
             <div className="flex items-center gap-2 sm:gap-4 relative">
